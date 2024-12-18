cac3f4bd2029f0aa3a06a82a8cafce116e3fee99
Temart00f <temerbalinovartur@mail.ru>
2024-12-18 23:55:11 +0600

    milk otbor

---
+++ Alg/sem_1/RGR/Otbor_v_razvedku/Otbor_v_razvedku.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Otbor_v_razvedku
+{
+    static int Group_Counter(int n)
+    {
+        if (n < 3 || n == 4) return 0;
+        if (n == 3) return 1;
+
+        int group_1 = n / 2;
+        int group_2 = n - group_1;
+
+        return Group_Counter(group_1) + Group_Counter(group_2);
+    }
+
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine(Group_Counter(n));
+    } 
+
+}
